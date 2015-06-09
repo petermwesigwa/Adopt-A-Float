d2a54581bea095e0f02566d48e1a35f0ba4cc6a8
Ben Leizman <bleizman@princeton.edu>
2015-06-08 21:54:42 -0400

    Initial Commit

---
+++ Adopt-A-Float/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Adopt-A-Float
+//
+//  Created by Ben Leizman on 6/8/15.
+//  Copyright (c) 2015 Son-O-Mermaid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Adopt-A-FloatTests/Adopt_A_FloatTests.m
@@ -0,0 +1,40 @@
+//
+//  Adopt_A_FloatTests.m
+//  Adopt-A-FloatTests
+//
+//  Created by Ben Leizman on 6/8/15.
+//  Copyright (c) 2015 Son-O-Mermaid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Adopt_A_FloatTests : XCTestCase
+
+@end
+
+@implementation Adopt_A_FloatTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
