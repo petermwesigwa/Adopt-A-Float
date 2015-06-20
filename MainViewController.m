9fae56bbf55b07f39dc6f08f5a2bc3b46a9ccdf2
Ben Leizman <bleizman@princeton.edu>
2015-06-20 13:17:01 -0400

    Map shows up!

--- MainViewController.m
+++ MainViewController.m
@@ -7,16 +7,36 @@
 //
 
 #import "MainViewController.h"
+#import <GoogleMaps/GoogleMaps.h>
 
 @interface MainViewController ()
 
 @end
 
-@implementation MainViewController
+@implementation MainViewController {
+    GMSMapView *mapView_;
+    __weak IBOutlet UIView *appMapView;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    // Create a GMSCameraPosition that tells the map to display the
+    // coordinate -33.86,151.20 at zoom level 6.
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
+                                                            longitude:151.20
+                                                                 zoom:6];
+    mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    mapView_.myLocationEnabled = YES;
+    appMapView = mapView_;
+    
+    // Creates a marker in the center of the map.
+    GMSMarker *marker = [[GMSMarker alloc] init];
+    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
+    marker.title = @"Sydney";
+    marker.snippet = @"Australia";
+    marker.map = mapView_;
 }
 
 - (void)didReceiveMemoryWarning {
