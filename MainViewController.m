eb7564482d19162b8db8da5072ce2fb2ddd28a5c
Ben Leizman <bleizman@princeton.edu>
2015-07-05 11:23:34 -0400

    - GMSMapView resizes to contain all of the markers

--- MainViewController.m
+++ MainViewController.m
@@ -9,6 +9,7 @@
 #import "MainViewController.h"
 #import "Instrument.h"
 #import <QuartzCore/QuartzCore.h>
+#import <math.h>
 
 extern NSMutableDictionary* instruments;
 
@@ -18,6 +19,7 @@ extern NSMutableDictionary* instruments;
     Instrument *curr;
     __weak IBOutlet UILabel *titleLabel;
     int defaultMarkerNumber;
+    NSMutableArray *onMarkers;
 }
 
 @end
@@ -47,12 +49,15 @@ extern NSMutableDictionary* instruments;
     
     // Create markers for all positions of all floats
     markers = [[NSMutableDictionary alloc] init];
+    onMarkers = [[NSMutableArray alloc] init];
     NSArray* instrumentNames = [instruments allKeys];
+    //UIImage *icon = [UIImage imageNamed:@"instrument"]; < ****this works, just need smaller image size
+    UIImage *icon = [UIImage imageWithContentsOfFile:@"returnTheDefault"];
     for(NSString*name in instrumentNames) {
         Instrument* instr = [instruments objectForKey:name];
         NSMutableArray* markersForInstr = [[NSMutableArray alloc] init];
         for (int i = 0; i < instr.lon.count; i++) { //count of lon and lat should be the same
-            GMSMarker* marker = [self createMarkerWithLat:instr.lat[i] andLong:instr.lon[i] andTitle:instr.name andSnippet:[NSString stringWithFormat:@"t-%d hours", i]];
+            GMSMarker* marker = [self createMarkerWithLat:instr.lat[i] andLong:instr.lon[i] andTitle:instr.name andSnippet:[NSString stringWithFormat:@"t-%d hours", i] andIcon:icon];
             [markersForInstr addObject:marker];
         }
         [markers setObject:markersForInstr forKey:name];
@@ -63,37 +68,76 @@ extern NSMutableDictionary* instruments;
     curr = [instruments allValues][0]; //Whichever instrument is the first in the array
     [self instrumentSetup];
     
-    // Create a GMSCameraPosition for the default instrument
-    Instrument* camIns;
-    NSLog(@"before getting raffa instrument");
-    camIns = [instruments objectForKey:@"raffa"];
-    float camLat = [camIns.lat[0] floatValue];
-    float camLon = [camIns.lon[0] floatValue];
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:camLat
-                                                            longitude:camLon
-                                                                 zoom:11];
+    // Create a GMSCameraPosition for the initial camera
+    NSLog(@"before setting the camera");
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:0.0
+                                                            longitude:0.0
+                                                                 zoom:1];
     mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
     mapView_.myLocationEnabled = YES;
     appMapView.camera = camera;
-    NSLog(@"After getting raffa instrument");
+    NSLog(@"After setting the camera");
+    
+    //Update the camera position
+    [self updateCameraPositionWithAnimation:NO];
 }
 
-- (GMSMarker*)createMarkerWithLat:(NSNumber*)lat andLong:(NSNumber*)lon andTitle:(NSString*)title andSnippet:(NSString*)snip {
+- (void) updateCameraPositionWithAnimation:(BOOL)animation {
+    double lonMin = MAXFLOAT;
+    double lonMax = -MAXFLOAT;
+    double latMin = MAXFLOAT;
+    double latMax = -MAXFLOAT;
+    for (GMSMarker *marker in onMarkers) {
+        NSLog(@"The comparison is happening");
+        if (marker.position.longitude < lonMin)
+            lonMin = marker.position.longitude;
+        if (marker.position.longitude > lonMax)
+            lonMax = marker.position.longitude;
+        if (marker.position.latitude < latMin)
+            latMin = marker.position.latitude;
+        if (marker.position.latitude > latMax)
+            latMax = marker.position.latitude;
+    }
+    NSLog(@"lonMin = %f", lonMin);
+    NSLog(@"lonMax = %f", lonMax);
+    NSLog(@"latMin = %f", latMin);
+    NSLog(@"latMax = %f", latMax);
+    //Potential bug in these coordinates, but don't know how to fix
+     //Relating to which coord is more east and which is more west (wraparound)
+    CLLocationCoordinate2D northEast = CLLocationCoordinate2DMake(latMax, lonMax);
+    CLLocationCoordinate2D southWest = CLLocationCoordinate2DMake(latMin, lonMin);
+    GMSCoordinateBounds *bounds = [[GMSCoordinateBounds alloc] initWithCoordinate:northEast coordinate:southWest];
+    GMSCameraUpdate *update = [GMSCameraUpdate fitBounds:bounds];
+    if (animation == NO)
+        [appMapView moveCamera:update];
+    else [appMapView animateWithCameraUpdate:update];
+}
+
+- (GMSMarker*)createMarkerWithLat:(NSNumber*)lat andLong:(NSNumber*)lon andTitle:(NSString*)title andSnippet:(NSString*)snip andIcon:(UIImage*)icon {
     GMSMarker *marker = [[GMSMarker alloc] init];
     marker.position = CLLocationCoordinate2DMake([lat floatValue], [lon floatValue]);
     marker.title = title;
     marker.snippet = snip;
     marker.map = nil;
+    marker.icon = icon;
     return marker;
 }
 
 - (void)turnOffMarker:(GMSMarker*) marker {
     marker.map = nil;
+    [onMarkers removeObject:marker];
 }
 
 - (void)turnOnMarker:(GMSMarker*) marker withOpacity:(float)opac {
-    marker.map = appMapView;
     marker.opacity = opac;
+    [onMarkers addObject:marker];
+}
+
+- (void) addOnMarkersToMap {
+    for (GMSMarker* marker in onMarkers) {
+        marker.map = appMapView;
+        marker.appearAnimation = YES;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -146,9 +190,16 @@ extern NSMutableDictionary* instruments;
         float opac = 1 - (i/(defaultMarkerNumber+1.0));
         [self turnOnMarker:[markers objectForKey:curr.name][i] withOpacity:opac];
     }
+    
     //Change label
     titleLabel.text = curr.name;
     NSLog(@"Current instrument is %@", curr.name);
+    
+    //Update the camera position
+    [self updateCameraPositionWithAnimation:YES];
+    
+    //Add on markers to the map
+    [self addOnMarkersToMap];
 }
 
 - (void) instrumentTakeDown:(Instrument*) old {
