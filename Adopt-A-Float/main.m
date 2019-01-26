2d9dcffcf8aba597b5313e6029d157fe9c75d73e
Peter Mwesigwa <mps1401@gmail.com>
2019-01-26 16:57:34 -0500

    edited a typo in main.m

--- Adopt-A-Float/main.m
+++ Adopt-A-Float/main.m
@@ -11,6 +11,6 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-p         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
