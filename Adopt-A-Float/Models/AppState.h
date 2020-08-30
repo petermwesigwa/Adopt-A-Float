18fb1ad1e5793abf8362138310fdba8bbe66f38e
petermwesigwa <mps1401@gmail.com>
2020-08-30 10:57:24 -0400

    changes to project

---
+++ Adopt-A-Float/Models/AppState.h
@@ -0,0 +1,17 @@
+//
+//  AppState.h
+//  Adopt-A-Float
+//
+//  Created by Peter Mwesigwa on 8/29/20.
+//  Copyright © 2020 Frederik Simons. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AppState : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Adopt-A-Float/Models/AppState.m
@@ -0,0 +1,13 @@
+//
+//  AppState.m
+//  Adopt-A-Float
+//
+//  Created by Peter Mwesigwa on 8/29/20.
+//  Copyright © 2020 Frederik Simons. All rights reserved.
+//
+
+#import "AppState.h"
+
+@implementation AppState
+
+@end
